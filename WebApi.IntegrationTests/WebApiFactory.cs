4f8ee8f33a6c5bb12062ecf7348c598611e77456
Venya Brodetskiy <benjamincheg@gmail.com>
2024-11-17 00:25:36 +0200

    added integration test with web application factory

---
+++ WebApi.IntegrationTests/WebApiFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using WebApi.Data;
+
+namespace WebApi.IntegrationTests;
+
+public class WebApiFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll(typeof(DbContextOptions<AppDbContext>));
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("Numbers");
+            });
+        });
+    }
+}
