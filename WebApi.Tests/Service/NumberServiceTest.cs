0cc98a017d7ab143542a7aa83012a8403b03c77b
Venya Brodetskiy <benjamincheg@gmail.com>
2024-11-16 23:26:48 +0200

    unit tests added

---
+++ WebApi.Tests/Service/NumberServiceTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using WebApi.Service;
+
+namespace WebApi.Tests.Service;
+
+public class NumberServiceTest
+{
+
+    [Fact]
+    public async Task CalculateAverageAsync_ReturnsCorrectSum_WhenNumbersExist()
+    {
+        // Arrange
+        var mockDbService = new Mock<IDbService>();
+        var mockLogger = new Mock<ILogger<NumberService>>();
+
+        var numbers = new List<int> { 10, 20, 30 };
+        mockDbService
+            .Setup(db => db.GetNumbersAsync())
+            .ReturnsAsync(numbers);
+        var numberService = new NumberService(mockDbService.Object, mockLogger.Object);
+
+        // Act
+        var result = await numberService.CalculateAverageAsync();
+
+        // Assert
+        Assert.Equal(numbers.Sum(), result);
+    }
+
+    [Fact]
+    public async Task CalculateAverageAsync_ReturnsZero_WhenNumbersDoNotExist()
+    {
+        // Arrange
+        var mockDbService = new Mock<IDbService>();
+        var mockLogger = new Mock<ILogger<NumberService>>();
+
+        var numbers = new List<int>();
+        mockDbService
+            .Setup(db => db.GetNumbersAsync())
+            .ReturnsAsync(numbers);
+        var numberService = new NumberService(mockDbService.Object, mockLogger.Object);
+
+        // Act
+        var result = await numberService.CalculateAverageAsync();
+
+        // Assert
+        Assert.Equal(0, result);
+    }
+}
