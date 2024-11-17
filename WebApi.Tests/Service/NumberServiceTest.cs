68a5c279e080d0341158847856ddfb55ae0d781a
Venya Brodetskiy <benjamincheg@gmail.com>
2024-11-17 18:15:09 +0200

    added nsubstitute

--- WebApi.Tests/Service/NumberServiceTest.cs
+++ WebApi.Tests/Service/NumberServiceTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using Moq;
+using NSubstitute;
 using WebApi.Service;
 
 namespace WebApi.Tests.Service;
@@ -11,20 +12,25 @@ public class NumberServiceTest
     public async Task CalculateAverageAsync_ReturnsCorrectSum_WhenNumbersExist()
     {
         // Arrange
-        var mockDbService = new Mock<IDbService>();
-        var mockLogger = new Mock<ILogger<NumberService>>();
+        //var mockDbService = new Mock<IDbService>();
+        var mockDbService = Substitute.For<IDbService>();
+        //var mockLogger = new Mock<ILogger<NumberService>>();
+        var mockLogger = Substitute.For<ILogger<NumberService>>();
 
         var numbers = new List<int> { 10, 20, 30 };
-        mockDbService
-            .Setup(db => db.GetNumbersAsync())
-            .ReturnsAsync(numbers);
-        var numberService = new NumberService(mockDbService.Object, mockLogger.Object);
+        //mockDbService
+        //    .Setup(db => db.GetNumbersAsync())
+        //    .ReturnsAsync(numbers);
+
+        mockDbService.GetNumbersAsync().Returns(numbers);
+
+        var numberService = new NumberService(mockDbService, mockLogger);
 
         // Act
         var result = await numberService.CalculateAverageAsync();
 
         // Assert
-        Assert.Equal(numbers.Sum(), result);
+        Assert.Equal(numbers.Average(), result);
     }
 
     [Fact]
