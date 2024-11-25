218f56b82ffec3baa4ead11aeb824148bc2e75d0
Venya Brodetskiy <benjamincheg@gmail.com>
2024-11-25 23:47:17 +0200

    builder patter - pizza class created

---
+++ BuilderPattern/Pizza.cs
@@ -0,0 +1,29 @@
+﻿namespace BuilderPattern;
+public class Pizza
+{
+    private string Size { get; set; }
+    private string Crust { get; set; }
+    private List<string> Toppings { get; set; }
+
+    public Pizza(string size, string crust, List<string> toppings)
+    {
+        if (string.IsNullOrWhiteSpace(size))
+            throw new ArgumentException("Size cannot be null or empty.");
+
+        if (string.IsNullOrWhiteSpace(crust))
+            throw new ArgumentException("Crust cannot be null or empty.");
+
+        if (toppings == null || toppings.Count == 0)
+            throw new ArgumentException("A pizza must have at least one topping.");
+
+        Size = size;
+        Crust = crust;
+        Toppings = toppings;
+    }
+
+    public string CookPizza()
+    {
+        return $"Cooked Pizza of Size: {Size}, Crust: {Crust}, Toppings: {string.Join(", ", Toppings)}";
+    }
+}
+
---
+++ BuilderPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BuilderPattern;
+
+var toppings = new List<string> { "Cheese", "Pepperoni", "Olives" };
+var pizza = new Pizza("Large", "Thin", toppings);
+
+var cookedPizza = pizza.CookPizza();
+Console.WriteLine(cookedPizza);
+
+var pizzaBuilder = new PizzaNew.PizzaBuilder();
+var pizzaNew = pizzaBuilder
+    .WithSize("Large")
+    .WithCrust("Thin")
+    .AddTopping("Cheese")
+    .AddTopping("Pepperoni")
+    .AddTopping("Olives")
+    .Build();
+
+var cookedPizzaNew = pizzaNew.CookPizza();
+Console.WriteLine(cookedPizzaNew);
