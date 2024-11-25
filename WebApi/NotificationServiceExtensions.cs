4c00a5815f06882e0bc04f8d962d90b77120f20a
Venya Brodetskiy <benjamincheg@gmail.com>
2024-11-26 00:15:34 +0200

    Refactor notification service registration

---
+++ WebApi/NotificationServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using WebApi.Service;
+
+namespace WebApi;
+
+public static class NotificationServiceExtensions
+{
+    public static IServiceCollection AddNotificationServices(this IServiceCollection services)
+    {
+        services.AddSingleton<EmailNotificationService>();
+        services.AddSingleton<SmsNotificationService>();
+        services.AddSingleton<PushNotificationService>();
+
+        services.AddSingleton<INotificationFactory, NotificationFactory>();
+
+        return services;
+    }
+}
