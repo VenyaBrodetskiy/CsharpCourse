26097d05b5b041eca9fdefff17c8486b3d498b2d
Venya Brodetskiy <benjamincheg@gmail.com>
2024-11-30 23:14:49 +0200

    Add WebApi.PerformanceTest project and NBomber tests
    In `Program.cs`, new code has been added to set up and run performance tests using the NBomber library. Two scenarios are created: one for a "Controller API" and another for a "Minimal API". Both scenarios include HTTP GET requests to specific endpoints and are configured with warm-up durations and load simulations.

---
+++ WebApi.PerformanceTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using NBomber.CSharp;
+using NBomber.Http.CSharp;
+
+var httpClient = HttpClientFactory.Create();
+
+var controllerApiScenario = Scenario.Create("Controller API", async _ =>
+{
+    var request = Http.CreateRequest(
+        HttpMethod.Get.ToString(), "http://localhost:5147/notify-mom/?type=push");
+
+    var response = await Http.Send(httpClient, request);
+
+    return response;
+})
+.WithWarmUpDuration(TimeSpan.FromSeconds(5))
+.WithLoadSimulations(Simulation.KeepConstant(20, TimeSpan.FromSeconds(30)));
+
+var minimalApiScenario = Scenario.Create("Minimal API", async _ =>
+{
+    var request = Http.CreateRequest(
+        HttpMethod.Get.ToString(), "http://localhost:5000/notify-mom/?type=push");
+    
+    var response = await Http.Send(httpClient, request);
+    
+    return response;
+})
+.WithWarmUpDuration(TimeSpan.FromSeconds(5))
+.WithLoadSimulations(Simulation.KeepConstant(20, TimeSpan.FromSeconds(30)));
+
+NBomberRunner
+    .RegisterScenarios(controllerApiScenario, minimalApiScenario)
+    .Run();
