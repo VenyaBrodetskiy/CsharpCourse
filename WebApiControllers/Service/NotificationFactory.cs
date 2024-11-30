d18cc6a2551fe2128fbc2b5f658743043ac1f8b1
Venya Brodetskiy <benjamincheg@gmail.com>
2024-11-30 22:00:05 +0200

    Add WebApiControllers project with services and controllers

---
+++ WebApiControllers/Service/NotificationFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApiControllers.Service;
+
+public interface INotificationFactory
+{
+    INotificationService GetNotificationService(string type);
+}
+public class NotificationFactory(IServiceProvider serviceProvider) : INotificationFactory
+{
+    public INotificationService GetNotificationService(string type)
+    {
+        return type.ToLower() switch
+        {
+            "email" => serviceProvider.GetRequiredService<EmailNotificationService>(),
+            "sms" => serviceProvider.GetRequiredService<SmsNotificationService>(),
+            "push" => serviceProvider.GetRequiredService<PushNotificationService>(),
+            _ => throw new NotSupportedException($"Notification type '{type}' is not supported")
+        };
+    }
+}
+
