4bab4642eec9272f685d405b8b4ed98420525eb3
Venya Brodetskiy <benjamincheg@gmail.com>
2024-12-08 16:21:14 +0200

    init enumerable project

---
+++ EnumerableEnumarator/Program.cs
@@ -0,0 +1,18 @@
+﻿using EnumerableEnumerator;
+
+var fibonacci = new FibonacciSequence(10);
+
+Console.WriteLine("Fibonacci numbers");
+foreach (var number in fibonacci)
+{
+    Console.WriteLine("Number: " + number);
+}
+
+// using enumerator
+Console.WriteLine("Fibonacci numbers using enumerator");
+using var enumerator = fibonacci.GetEnumerator();
+    while (enumerator.MoveNext())
+    {
+        Console.WriteLine("Number: " + enumerator.Current);
+    }
+}
