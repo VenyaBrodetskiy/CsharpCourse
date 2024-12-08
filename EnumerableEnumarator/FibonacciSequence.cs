e2a5ec04b11c9e8de485487d696e87efb8ae1d32
Venya Brodetskiy <benjamincheg@gmail.com>
2024-12-08 16:27:06 +0200

    Implement Fibonacci sequence and enumerator classes

--- EnumerableEnumarator/FibonacciSequence.cs
+++ EnumerableEnumarator/FibonacciSequence.cs
@@ -1,5 +1,23 @@
-﻿namespace EnumerableEnumerator;
+﻿using System.Collections;
+
+namespace EnumerableEnumerator;
 
 internal class FibonacciSequence : IEnumerable<int>
 {
+    private readonly int _count;
+
+    public FibonacciSequence(int count)
+    {
+        _count = count;
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        return new FibonacciEnumerator(_count);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
 }
--- EnumerableEnumarator/Program.cs
+++ EnumerableEnumarator/Program.cs
@@ -8,11 +8,13 @@ foreach (var number in fibonacci)
     Console.WriteLine("Number: " + number);
 }
 
+Console.WriteLine("Access fibonacci number by index");
+Console.WriteLine("Number at index 7: " + fibonacci[7]);
+
 // using enumerator
 Console.WriteLine("Fibonacci numbers using enumerator");
 using var enumerator = fibonacci.GetEnumerator();
-    while (enumerator.MoveNext())
-    {
-        Console.WriteLine("Number: " + enumerator.Current);
-    }
+while (enumerator.MoveNext())
+{
+    Console.WriteLine("Number: " + enumerator.Current);
 }
