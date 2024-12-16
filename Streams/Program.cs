cb9b723c2bd7e918813a5b11adc787e86d72c4e1
Venya Brodetskiy <benjamincheg@gmail.com>
2024-12-17 00:14:09 +0200

    add streams project

---
+++ Streams/Program.cs
@@ -0,0 +1,16 @@
+﻿
+File.WriteAllText("file1.txt", "Hello, World");
+
+var content = File.ReadAllText("file1.txt");
+
+Console.WriteLine(content);
+
+Console.WriteLine("Press any key to start");
+
+Console.ReadKey(); 
+var video = File.ReadAllBytes("test-video.mp4");
+
+Console.WriteLine($"Read file with size: {(double)video.Length / 1024 / 1024 / 1024 :F2} GB");
+
+Console.WriteLine("Press any key to finish");
+Console.ReadKey();
